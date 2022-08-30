c2387c9dd9461e17fd37e5aee520f92e117585e5
jonardmondero <62581989+jonardmondero@users.noreply.github.com>
2022-08-30 16:27:46 +0800

    push

--- admin 2/employees.php
+++ admin 2/employees.php
@@ -15,301 +15,316 @@ $prep_emp->execute();
 <html>
 
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+    <div class="wrapper">
 
-  <!-- Navbar -->
- <?php include ('dtrdesign/navbar.php');
+        <!-- Navbar -->
+        <?php include ('dtrdesign/navbar.php');
   include('dtrdesign/sidebar.php');
 ?>
-      
-  <!-- /.navbar -->
 
-  <!-- Main Sidebar Container -->
-  
+        <!-- /.navbar -->
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row col-12">
-         <!--  <div class="col-sm-6">
+        <!-- Main Sidebar Container -->
+
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row col-12">
+                        <!--  <div class="col-sm-6">
             <h1 class="m-0 text-dark">Employees</h1><br>
               
-          </div>--> 
-          <div class ="col-2">
-            <button class = "btn btn-primary" id = "addemp" data-toggle="modal" data-target="#addemployee">Add Employee </button>
-          </div>
-           <div class="form-group has-feedback col-8">
-         <?php echo $alert_msg; ?>      
-      </div>
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
+          </div>-->
+                        <div class="col-2">
+                            <button class="btn btn-primary" id="addemp" data-toggle="modal"
+                                data-target="#addemployee"><i class="fa fa-plus"></i> </button>
+                        </div>
+                        <div class="form-group has-feedback col-8">
+                            <?php echo $alert_msg; ?>
+                        </div>
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+
+
+                    <!-- Small boxes (Stat box) -->
+                    <div class="col-12">
+                        <div class="row">
+                            <div class="col-12 wrapper">
+                                <div class="card card-primary">
+                                    <div class="card-header">
+                                        <h3 class="card-title">Employees Table</h3>
+                                    </div>
+                                    <div class="card-body">
+
+
+                                        <div class="row">
+                                            <?php include('elements/tbl_employee.php')?>
+
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <?php include('modal/add_employee_modal.php');?>
+            <?php include('modal/add_worksched_modal.php');?>
+            <?php include('modal/update_supervisor_modal.php');?>
+
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+        <footer class="main-footer">
+            <strong>Copyright &copy; 2014-2018 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+            All rights reserved.
+            <div class="float-right d-none d-sm-inline-block">
+                <b>Version</b> 3.0.0-alpha
+            </div>
+        </footer>
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
     </div>
-    <!-- /.content-header -->
+    <!-- ./wrapper -->
 
-    <!-- Main content -->
-    <section class="content">
-        <div class="container-fluid">
-           
-        
-        <!-- Small boxes (Stat box) -->
-       <div class="col-12">
-        <div class = "row">
-          <div class ="col-12 wrapper" >
-          <div class="card card-primary">
-               <div class="card-header">
-                <h3 class="card-title">Employees Table</h3>
-              </div>
-              <div class = "card-body">
-              
-     
-              <div class = "row">
-             <?php include('elements/tbl_employee.php')?>
-            
-        
-           </div>
-       </div>
-         </div>
-         </div>
-           </div>
-           </div>
-           </div>
-    </section>
-    <?php include('modal/add_employee_modal.php');?>
-    <?php include('modal/add_worksched_modal.php');?>
-   <?php include('modal/update_supervisor_modal.php');?>
+    <!-- jQuery -->
+    <?php include('dtrdesign/footer.php');?>
 
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-  <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2018 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.0.0-alpha
-    </div>
-  </footer>
+    <script>
+    $(document).ready(function() {
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
+        sel_worksched();
 
-<!-- jQuery -->
-<?php include('dtrdesign/footer.php');?>
+        function is_valid(element) {
+            // callback function
+            // returns every value
+            return element.value;
+        }
 
-<script>
-	$(document).ready(function(){
-     
-  sel_worksched();
-	 function is_valid(element){
-      // callback function
-      // returns every value
-      return element.value;
-    }
-     function post_notify(message, type){
+        function post_notify(message, type) {
 
-      if (type == 'success') {
+            if (type == 'success') {
 
-        $.notify({
-          message: message
-        },{
-          type: 'success',
-          delay: 2000
+                $.notify({
+                    message: message
+                }, {
+                    type: 'success',
+                    delay: 2000
+                });
+
+            } else {
+
+                $.notify({
+                    message: message
+                }, {
+                    type: 'danger',
+                    delay: 2000
+                });
+
+            }
+
+        }
+
+        function reset_form_input(form_id) {
+            $('#' + form_id).each(function() {
+                this.reset();
+            });
+        }
+
+
+
+        $('#empnum').change(function() {
+            var emp = $('#empnum').val();
+
+            $.ajax({
+                type: "POST",
+                url: "ajaxcall/check_empnumber.php",
+                data: {
+                    empnum: emp
+                },
+                success: function(response) {
+                    var result = jQuery.parseJSON(response);
+                    if (result.data1 != '') {
+                        $('#checkempid').html('This employee number is already taken.');
+                        $('#save').prop('disabled', true);
+                        // console.log(result.data1);
+                    } else {
+                        if (emp != '') {
+                            $('#checkempid').html('This employee number is available.');
+                            $('#save').prop('disabled', false);
+                        }
+                    }
+                },
+                error: function(xhr, b, c) {
+                    console.log("xhr=" + xhr.responseText + " b=" + b.responseText + " c=" +
+                        c.responseText);
+                }
+            })
+            if (emp == '') {
+                $('#checkempid').html('');
+                $('#save').prop('disabled', false);
+            }
+
+
+        })
+        //check the biometric id if it is available.
+        $('#bioid').change(function() {
+            var bio = $('#bioid').val();
+
+            $.ajax({
+                type: "POST",
+                url: "ajaxcall/check_empnumber.php",
+                data: {
+                    bioid: bio
+                },
+                success: function(response) {
+                    var result = jQuery.parseJSON(response);
+                    if (result.data2 != '') {
+                        $('#checkbioid').html('This Biometric ID is already taken.');
+                        $('#save').prop('disabled', true);
+
+                    } else {
+                        if (bio != '') {
+                            $('#checkbioid').html('This Biometric ID is available.');
+                            $('#save').prop('disabled', false);
+                        }
+                    }
+                },
+                error: function(xhr, b, c) {
+                    console.log("xhr=" + xhr.responseText + " b=" + b.responseText + " c=" +
+                        c.responseText);
+                }
+            })
+            if (bio == '') {
+                $('#checkbioid').html('');
+                $('#save').prop('disabled', false);
+            }
+
+
+        })
+        $('#addemp').click(function() {
+            reset_form_input('employee-form');
+            $('#checkbioid').html('');
+            $('#checkempid').html('');
+            $("#empnum").prop('disabled', false);
+            $('#insert').prop('hidden', false);
+            $('#delete').prop('hidden', true);
+            $('#update').prop('hidden', true);
+            $("#empnum").prop('readonly', false);
+
+            $('#department').select2({
+                dropdownParent: $('#addemployee')
+            });
+
+        })
+
+        $('#table_employee tbody').on('click', '.add_worksched', function() {
+            event.preventDefault();
+
+            var currow = $(this).closest('tr');
+            var col1 = currow.find('td:eq(0)').text();
+            var col2 = currow.find('td:eq(5)').text();
+            console.log(col2);
+
+            // var id = $(this).data('id');
+            $('#addemployeesched').modal('show');
+            $(`#sel_worksched option[value='${col2}']`).prop('selected', 'true');
+            $('#empno').val(col1);
+
+
+            $('#sel_worksched').select2({
+                dropdownParent: $('#addemployeesched')
+            });
+            sel_worksched();
+        })
+
+
+
+        function sel_worksched() {
+            var worksched = $('#sel_worksched').val();
+            $('#work_body').load('ajaxcall/get_workschedule.php', {
+                    workcode: worksched
+                },
+                function(response, status, xhr) {
+                    if (status == "error") {
+                        alert(msg + xhr.status + " " + xhr.statusText);
+                        console.log(msg + xhr.status + " " + xhr.statusText);
+                    }
+                });
+        }
+
+
+
+        $('#sel_worksched').change(function() {
+            sel_worksched();
+        })
+
+
+
+
+
+        $("#table_employee tbody").on("click", ".edit_employee", function() {
+            //SHOWS THE ADD EMPLOYEE MODAL AND DISPLAY THE EMPLOYEE'S INFORMATION
+            event.preventDefault();
+            $("#addemployee").modal('show');
+            $('#insert').hide();
+            $('#update').prop('hidden', false);
+            $('#delete').prop('hidden', false);
+            $("#empnum").prop('readonly', true);
+            var currow = $(this).closest('tr');
+            var col1 = currow.find('td:eq(0)').text();
+            console.log('hello');
+            $.ajax({
+
+                url: 'ajaxcall/get_employee.php',
+                type: 'POST',
+                data: {
+                    id: col1
+                },
+                success: function(response) {
+                    var result = jQuery.parseJSON(response);
+                    $('#empnum').val(result.employeeno);
+                    $('#lname').val(result.lastname);
+                    $('#fname').val(result.firstname);
+                    $('#mname').val(result.middlename);
+                    $('#bioid').val(result.bioid);
+                    $('#department').val(result.department);
+                    $(`#department option[value='${result.department}']`).prop('selected',
+                        true);
+                    $('#estatus').val(result.employmentstatus);
+                    $('#supervisor').val(result.supervisor);
+                    $('#status').val(result.status);
+                    $('#worksched').val(result.worksched);
+
+                    $('#department').select2({
+                        dropdownParent: $('#addemployee')
+                    });
+                },
+                error: function(xhr, b, c) {
+                    console.log("xhr=" + xhr.responseText + " b=" + b.responseText + " c=" +
+                        c.responseText);
+                }
+
+            })
+
         });
 
-      } else{
 
-        $.notify({
-          message: message
-        },{
-          type: 'danger',
-          delay: 2000
-        }); 
-
-      }
-
-    }
-function reset_form_input(form_id){
-      $( '#'+form_id ).each(function(){
-          this.reset();
-      });
-    }
-    
-
-	
-	$('#empnum').change(function(){
-  var emp = $('#empnum').val();
- 
-$.ajax({
-  type:"POST",
-  url:"ajaxcall/check_empnumber.php",
-  data:{empnum:emp},
-  success: function(response){
-    var result = jQuery.parseJSON(response);
-    if(result.data1!= ''){
-      $('#checkempid').html('This employee number is already taken.');
-      $('#save').prop('disabled', true);
-// console.log(result.data1);
-}
-else{
-  if(emp !=''){
-    $('#checkempid').html('This employee number is available.');
-    $('#save').prop('disabled', false);
-}
-}
-  },
-  error: function (xhr, b, c) {
-   console.log("xhr=" + xhr.responseText + " b=" + b.responseText + " c=" + c.responseText);
-     }
-})
-if(emp == ''){
-  $('#checkempid').html('');
-  $('#save').prop('disabled', false);
-}
-
-
-})
-//check the biometric id if it is available.
-$('#bioid').change(function(){
-  var bio = $('#bioid').val();
- 
-$.ajax({
-  type:"POST",
-  url:"ajaxcall/check_empnumber.php",
-  data:{bioid:bio},
-  success: function(response){
-    var result = jQuery.parseJSON(response);
-    if(result.data2!= ''){
-      $('#checkbioid').html('This Biometric ID is already taken.');
-      $('#save').prop('disabled', true);
-
-}
-else{
-  if(bio !=''){
-    $('#checkbioid').html('This Biometric ID is available.');
-    $('#save').prop('disabled', false);
-}
-}
-  },
-  error: function (xhr, b, c) {
-   console.log("xhr=" + xhr.responseText + " b=" + b.responseText + " c=" + c.responseText);
-     }
-})
-if(bio == ''){
-  $('#checkbioid').html('');
-  $('#save').prop('disabled', false);
-}
-
-
-})
-$('#addemp').click(function(){
-reset_form_input('employee-form');
-$('#checkbioid').html('');
-$('#checkempid').html('');
-$("#empnum").prop('disabled', false);
-$('#insert').prop('hidden',false);
-$('#delete').prop('hidden',true);
-$('#update').prop('hidden',true);
-$("#empnum").prop('readonly', false);
-
-$('#department').select2({
-        dropdownParent: $('#addemployee')
-      });
-
-})  
-
-$('#table_employee tbody').on( 'click', '.add_worksched', function(){
-        event.preventDefault();
-     
-  var currow=  $(this).closest('tr');
-  var col1 = currow.find('td:eq(0)').text();
-  var col2 = currow.find('td:eq(5)').text();
-  console.log(col2);
- 
-// var id = $(this).data('id');
-$('#addemployeesched').modal('show');
-$(`#sel_worksched option[value='${col2}']`).prop('selected', 'true');
-$('#empno').val(col1);
-
-
-$('#sel_worksched').select2({
-        dropdownParent: $('#addemployeesched')
-      });
-      sel_worksched();
-})  
-   
-
-
-function sel_worksched(){
-    var worksched = $('#sel_worksched').val();
-  $('#work_body').load('ajaxcall/get_workschedule.php',{workcode:worksched},
-   function(response, status, xhr) {
-  if (status == "error") {
-      alert(msg + xhr.status + " " + xhr.statusText);
-      console.log(msg + xhr.status + " " + xhr.statusText);
-  }
-});
-  }  
-
-
-
-$('#sel_worksched').change(function(){
- sel_worksched();
-})
-
-
-
-
-
-$("#table_employee tbody").on("click", ".edit_employee", function () {
-  //SHOWS THE ADD EMPLOYEE MODAL AND DISPLAY THE EMPLOYEE'S INFORMATION
-    event.preventDefault();
-    $("#addemployee").modal('show');
-    $('#insert').hide();
-    $('#update').prop('hidden',false);
-    $('#delete').prop('hidden',false);
-    $("#empnum").prop('readonly', true);
-    var currow=  $(this).closest('tr');
-    var col1 = currow.find('td:eq(0)').text();
-    console.log('hello');
-    $.ajax({
-
-        url:'ajaxcall/get_employee.php',
-        type:'POST',
-        data:{id:col1},
-        success:function(response){
-        var result = jQuery.parseJSON(response);
-        $('#empnum').val(result.employeeno);
-        $('#lname').val(result.lastname);
-        $('#fname').val(result.firstname);
-        $('#mname').val(result.middlename);
-        $('#bioid').val(result.bioid);
-        $('#department').val(result.department);
-        $(`#department option[value='${result.department}']`).prop('selected', true);
-        $('#estatus').val(result.employmentstatus);
-        $('#supervisor').val(result.supervisor);
-        $('#status').val(result.status);
-        $('#worksched').val(result.worksched);
-
-        $('#department').select2({
-        dropdownParent: $('#addemployee')
-      });
-        },
-        error: function (xhr, b, c) {
-     console.log("xhr=" + xhr.responseText + " b=" + b.responseText + " c=" + c.responseText);
-       }
-
-    })
-   
-});
-
-
-  });
-</script>
+    });
+    </script>
 </body>
+
 </html>
--- admin 2/modal/add_employee_modal.php
+++ admin 2/modal/add_employee_modal.php
@@ -3,31 +3,34 @@
 include('./components/textfields.php');
 ?>
 
- <div class="modal fade" id="addemployee">
-        <div class="modal-dialog">
-          <div class="modal-content">
+<div class="modal fade" id="addemployee">
+    <div class="modal-dialog">
+        <div class="modal-content">
             <div class="modal-header">
 
-              <h4 class="modal-title">Add Employee</h4>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
+                <h4 class="modal-title">Add Employee</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
-           
+
             <div class="modal-body">
-               <form class="form-horizontal" method = "post" action = "<?php htmlspecialchars("PHP_SELF");?>" id = "employee-form">
-              
-                <div class ="col-12">
-                <?php textField('Emp No:','checkempid','empnum');
+                <form class="form-horizontal" method="post" action="<?php htmlspecialchars("PHP_SELF");?>"
+                    id="employee-form">
+
+                    <div class="col-12">
+                        <?php textField('Emp No:','checkempid','empnum');
                       textField('Last Name:','lname','lname');  
                       textField('First Name:','fname','fname');
                       textField('Mdle Name:','mname','mname');   
                       textField('Bio I.D.:','bioid','bioid'); ?>
 
-                <div class = "form-group row">
-                 <label class = "col-form-label col-2"> Dept:  </label>
-                <select class ="form-control select2 col-5" style = "width:80%" name = "department" id ="department">
-                 <?php
+                        <div class="form-group row">
+                            <label class="col-form-label" style="margin-right:70px;margin-left:5px;"> Dept: </label>
+                            <select class="form-control select2 col-5" style="width:75%;height:50px;" name="department"
+                                id="department">
+                                <option value="Select Department" selected> Select Department </option>
+                                <?php
                      include('../config/config.php');
                      $get_user_sql = "SELECT * FROM department WHERE status = 'Active'";
                      $user_data = $con->prepare($get_user_sql);  
@@ -38,55 +41,55 @@ include('./components/textfields.php');
                         echo "<option value='".$deptId."'>".$deptdesc."</option>";
                     }
                    ?>
-                </select>
-                </div>
-              
-               <div class = "form-group row">
-                 <label class = "col-form-label col-5"> Employment Status:  </label>
-                <select class ="form-control col-7" name = "estatus" id ="estatus">
-                  <option val = "Regular">Regular</option>
-                  <option val ="Job Order">Job Order </option>
-                </select>
-                </div>
-                <?php textField(' Supervisor:','supervisor','supervisor');?>
-                <div class = "form-group row">
-                 <label class = "col-form-label col-5">Has Work Schedule?  </label>
-                <select class ="form-control col-7" name = "worksched" id ="worksched">
-                  <option val = "Yes">Yes</option>
-                  <option val = "No">No </option>
-                </select>
-                </div>
+                            </select>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-form-label col-5"> Employment Status: </label>
+                            <select class="form-control col-7" name="estatus" id="estatus">
+                                <option val="Regular">Regular</option>
+                                <option val="Job Order">Job Order </option>
+                            </select>
+                        </div>
+                        <?php textField(' Supervisor:','supervisor','supervisor');?>
+                        <div class="form-group row">
+                            <label class="col-form-label col-5">Has Work Schedule? </label>
+                            <select class="form-control col-7" name="worksched" id="worksched">
+                                <option val="Yes">Yes</option>
+                                <option val="No">No </option>
+                            </select>
+                        </div>
 
 
 
-                <div class = "form-group row">
-                 <label class = "col-form-label col-5">Status:  </label>
-                <select class ="form-control col-7" name = "status" id ="status">
-                  <option val = "Active">Active</option>
-                  <option val = "Not Active">Not Active </option>
-                </select>
-                </div>
-               
-         
-        
-        
-          
-          </div>
-            <div class="modal-footer justify-content-between">
-              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-              <button type="submit"   name = "delete" id = "delete" class="btn btn-danger">Delete</button>
-              <button type="submit"   name = "save" id = "insert" class="btn btn-primary">Save</button>
-              <button type="submit"   name = "update" id = "update" class="btn btn-primary">Update</button>
+                        <div class="form-group row">
+                            <label class="col-form-label col-5">Status: </label>
+                            <select class="form-control col-7" name="status" id="status">
+                                <option val="Active">Active</option>
+                                <option val="Not Active">Not Active </option>
+                            </select>
+                        </div>
+
+
+
+
+
+                    </div>
+                    <div class="modal-footer justify-content-between">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="submit" name="delete" id="delete" class="btn btn-danger">Delete</button>
+                        <button type="submit" name="save" id="insert" class="btn btn-primary">Save</button>
+                        <button type="submit" name="update" id="update" class="btn btn-primary">Update</button>
+                    </div>
+                </form>
             </div>
-             </form>
-              </div>
-          </div>
-          <!-- /.modal-content -->
         </div>
-        <!-- /.modal-dialog -->
-      </div>
-      <style>
-        .label {
-          margin-right:10px;
-        }
-      </style>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<style>
+.label {
+    margin-right: 10px;
+}
+</style>
