5633cc3c6f3692d4765e253c5707d8d9f5a4284b
jonardmondero <62581989+jonardmondero@users.noreply.github.com>
2022-09-11 21:06:01 +0800

    SET THE FIELDS OF EMPLOYEE INTO CAPITAL LETTERS

--- admin 2/components/textfields.php
+++ admin 2/components/textfields.php
@@ -3,9 +3,8 @@
 function textField($label,$labelid,$name){
     echo '<div class = "form-group row">';
     echo '<label class = " col-form-label col-3"> '.$label.'  </label>';
-    echo '<input type = "text" class = "form-control col-9 " required id = "'.$name.'" name = "'.$name.'">';
+    echo '<input type = "text" class = "form-control col-9 " onkeyup="this.value = this.value.toUpperCase();" required id = "'.$name.'" name = "'.$name.'" >';
     echo '<label id = "'.$labelid.'" style = "margin-left:115px;padding-top:5px;font-size:13px;color:red;font:Arial;"></label>';
     echo '</div>';
   }
   ?>
-  
