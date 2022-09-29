9c8d3b1d7c867d0658ee4b34bf715695813f9473
jonardmondero <62581989+jonardmondero@users.noreply.github.com>
2022-09-29 21:23:28 +0800

    update

--- admin 2/elements/dtr_table.php
+++ admin 2/elements/dtr_table.php
@@ -1,86 +1,89 @@
 <div class="col-8" style="margin-top:10px;">
-    <div class="card">
-        <div class="wrapper">
-            <div class="card-header">
-                <div class="row" style="margin:auto;">
-                    <div class="input-group col-6">
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="card bg-light">
+
+                <div class="card-header ">
+                    <div class="row  d-flex justify-content-center" style="margin:auto;">
+                        <div class="col-6 input-group ">
 
 
-                        <span class="input-group-text">
-                            <label style="padding-right:10px;padding-left: 10px">From: </label>
-                            <div style="padding-right:10px" class="input-group-addon">
-                                <i class="fa fa-calendar"></i>
-                            </div>
-                        </span>
-                        <input style="margin-right:10px;" type="text" class="form-control" style="font-size:13px"
-                            autocomplete="off" name="daterange" id="dtefrom" value="<?php echo $dteFrom;?>">
-
-                    </div>
-
-                    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#printDtr"
-                        aria-expanded="false" aria-controls="multiCollapseExample2"><i class="fa fa-print"></i>
-                    </button>
-
-                </div>
-
-                <div class="collapse multi-collapse" id="printDtr" style="margin-top:10px;">
-                    <div class="card card-body">
-                        <?php include ('modal/print_modal.php');?>
-                    </div>
-                </div>
-            </div>
-            <div class="card-body">
+                            <span class="input-group-text">
+                                <label style="padding-right:10px;padding-left: 10px">Date: </label>
+                                <div style="padding-right:10px" class="input-group-addon">
+                                    <i class="fa fa-calendar"></i>
+                                </div>
+                            </span>
+                            <input style="margin-right:10px;" type="text" class="form-control" style="font-size:13px"
+                                autocomplete="off" name="daterange" id="dtefrom" value="<?php echo $dteFrom;?>">
 
 
 
-
-                <form role="form" method="POST" action="<?php htmlspecialchars("PHP_SELF");?>">
-                    <div class="row" style="margin-top:5px;">
-                        <div class="col-3"></div>
-                        <div class="col-6" style="background-color:white-grey;">
-
-                            <div class="card full_name">
-                                <h4 id="full-name"> </h4>
-                            </div>
-                            <input type="hidden" readonly id="hiddenempno" name="hiddenempno"
-                                value="<?php echo $hidden;?>">
-                            <input type="hidden" readonly id="hiddendeptid" name="hiddendeptid" value="">
-
-                        </div>
-                        <div class="col-3"></div>
-                    </div>
-
-                    <div class="row col-12">
-
-                        <div style="margin:auto;">
-                            <table id="dtr" class="form__table table-bordered table-responsive table-hover ">
-                                <thead class="table">
-
-                                    <th class="border border-dark ">Date </th>
-                                    <th class="border border-dark">Day</th>
-                                    <th class="border border-dark">Check In</th>
-                                    <th class="border border-dark">Break Out</th>
-                                    <th class="border border-dark">Break In</th>
-                                    <th class="border border-dark">Check Out</th>
-                                    <th class="border border-dark">OT In</th>
-                                    <th class="border border-dark">OT Out</th>
-                                    <th class="border border-dark">Late</th>
-                                    <th class="border border-dark">UT</th>
-                                    <th class="border border-dark">Options</th>
-
-                                </thead>
-                                <tbody id="dtrbody">
-                                </tbody>
-                            </table>
-                  
+                            <button class="btn btn-primary float-right" type="button" data-toggle="collapse"
+                                data-target="#printDtr" aria-expanded="false" aria-controls="multiCollapseExample2"><i
+                                    class="fa fa-print"></i>
+                            </button>
 
                         </div>
                     </div>
-            
+
+                    <div class="collapse multi-collapse" id="printDtr" style="margin-top:10px;">
+                        <div class="card card-body">
+                            <?php include ('modal/print_modal.php');?>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+
+                        <form role="form" method="POST" action="<?php htmlspecialchars("PHP_SELF");?>">
+                            <div class="row" style="margin-top:5px;">
+                                <div class="col-3"></div>
+                                <div class="col-6" style="background-color:white-grey;">
+
+                                    <div class="card full_name">
+                                        <h4 id="full-name"> </h4>
+                                    </div>
+                                    <input type="hidden" readonly id="hiddenempno" name="hiddenempno"
+                                        value="<?php echo $hidden;?>">
+                                    <input type="hidden" readonly id="hiddendeptid" name="hiddendeptid" value="">
+
+                                </div>
+                                <div class="col-3"></div>
+                            </div>
+
+                            <div class="row col-12">
+
+                                <div style="margin:auto;">
+                                    <table id="dtr" class="form__table table-bordered table-responsive table-hover ">
+                                        <thead class="table">
+
+                                            <th class="border border-dark ">Date </th>
+                                            <th class="border border-dark">Day</th>
+                                            <th class="border border-dark">Check In</th>
+                                            <th class="border border-dark">Break Out</th>
+                                            <th class="border border-dark">Break In</th>
+                                            <th class="border border-dark">Check Out</th>
+                                            <th class="border border-dark">OT In</th>
+                                            <th class="border border-dark">OT Out</th>
+                                            <th class="border border-dark">Late</th>
+                                            <th class="border border-dark">UT</th>
+                                            <th class="border border-dark">Options</th>
+
+                                        </thead>
+                                        <tbody id="dtrbody">
+                                        </tbody>
+                                    </table>
+
+
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+
+
+                </div>
             </div>
-
-
-
-            </form>
         </div>
     </div>
--- admin 2/elements/employee_table.php
+++ admin 2/elements/employee_table.php
@@ -8,57 +8,59 @@ if($user_type == 'User'){
 }
 ?>
 
+<div class="wrapper">
+    <div class=" card card-primary" style="margin-top:10px;">
 
-<div class=" card card-primary"  style ="margin-top:10px;">
-<div class = "wrapper">
-  <div class = "container-fluid">
-               <div class="card-header">
-                <h3 class="card-title capitalize" >Department</h3>
-              </div>
-                <div class="card-body">
-                    <div class = "row" style="margin-bottom:20px;height:30px;">
-                        <div class = "col-12 ">
-                          
-                <select  style = "height:1em;"  class="select2 form-control"  name="department" id = "deptId" >
-                <?php
+        <div class="container-fluid">
+            <div class="card-header">
+                <h3 class="card-title capitalize">Department</h3>
+            </div>
+            <div class="card-body">
+                <div class="row" style="margin-bottom:20px;height:30px;">
+                    <div class="col-12 ">
+
+                        <select style="height:1em;" class="select2 form-control" name="department" id="deptId">
+                            <?php
              
                      $get_user_sql = "SELECT * FROM department WHERE status = 'Active'";
                      $user_data = $con->prepare($get_user_sql);  
                      $user_data->execute();
                         while ($result = $user_data->fetch(PDO::FETCH_ASSOC)) {?>
-                      
-                        <option value='<?php echo $result['deptId'];?>'><?php echo  $deptdesc = $result['departmentDescription'];?></option>;
-                    <?php }?>
-                </select>
-            </div>
-            
-              </div>
-                <div class = "row" style= "margin:auto;margin-bottom:20px;margin-top:30px;">
-                <select class = "form-control"  id = "emp_status" <?php echo $setreadonly;?>>
-                <option <?php echo $setSelected;?>  val= "Regular">Regular</option>
-                <option <?php echo $setSelected;?> val= "Job Order">Job Order</option>
-                </select>
-                </div>     
-             <div class = "row" id = "slim">  
-                 <div class = "col-12"  >
-            <table id = "employees" class = "table table-bordered table-hover dataTable" role = "grid" >
-            <thead>
-            <th> Employee No. </th>
-                <th> Full Name </th>
-                      </thead>              
-                        
-                <tbody id = "body"> 
-                    <tr id = "row" >              
-                  </tr>
-       
-           </tbody>
-                 
-                  
-             </table>
-              </div>
-                 </div>   
 
-                     </div>
-                     </div>
-                        </div>
-        </div>  
+                            <option value='<?php echo $result['deptId'];?>'>
+                                <?php echo  $deptdesc = $result['departmentDescription'];?></option>;
+                            <?php }?>
+                        </select>
+                    </div>
+
+                </div>
+                <div class="row" style="margin:auto;margin-bottom:20px;margin-top:30px;">
+                    <select class="form-control" id="emp_status" <?php echo $setreadonly;?>>
+                        <option <?php echo $setSelected;?> val="Regular">Regular</option>
+                        <option <?php echo $setSelected;?> val="Job Order">Job Order</option>
+                    </select>
+                </div>
+                <div class="row" id="slim">
+                    <div class="col-12">
+                        <table id="employees" class="table table-bordered table-reponsive table-hover dataTable"
+                            role="grid">
+                            <thead>
+                                <th> Employee No. </th>
+                                <th> Full Name </th>
+                            </thead>
+
+                            <tbody id="body">
+                                <tr id="row">
+                                </tr>
+
+                            </tbody>
+
+
+                        </table>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
