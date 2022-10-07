f37cc8a2052ecbd73d14fbb2f355394a8aecd297
jonardmondero <62581989+jonardmondero@users.noreply.github.com>
2022-10-07 13:05:51 +0800

    update

--- sql backup/view/viewbioinfo.sql
+++ sql backup/view/viewbioinfo.sql
@@ -4,14 +4,16 @@ USE `pmis`$$
 
 DROP VIEW IF EXISTS `viewbioinfo`$$
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`` SQL SECURITY DEFINER VIEW `viewbioinfo` AS (
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `viewbioinfo` AS (
 SELECT
   `b`.`employeeNo`     AS `employeeNo`,
   `b`.`biometricId`    AS `biometricId`,
   `b`.`workScheduleId` AS `workScheduleId`,
   `b`.`department`     AS `department`,
   `d`.`connection`     AS `connection`,
-  `b`.`schedule`       AS `schedule`
+  `b`.`schedule`       AS `schedule`,
+   `b`.`status`         AS `bio_status`,
+   `d`.`status`         AS `dep_status`
 FROM (`bioinfo` `b`
    JOIN `department` `d`
      ON (`b`.`department` = `d`.`deptId`)))$$
